6b42420b2ab5d03b5e0d7e3614c0cf07c0014e5a
Ivan Chistyakov <ivan.chkv@gmail.com>
2015-12-26 10:35:51 +0300

    fix InvalidModelExceptionTest

--- tests/base/InvalidModelExceptionTest.php
+++ tests/base/InvalidModelExceptionTest.php
@@ -2,9 +2,10 @@
 
 namespace yii\boost\tests\base;
 
+use Exception;
 use yii\boost\base\InvalidModelException;
 use yii\codeception\TestCase;
-use Yii;
+use yii\boost\tests\TestForm;
 
 class InvalidModelExceptionTest extends TestCase
 {
@@ -14,7 +15,57 @@ class InvalidModelExceptionTest extends TestCase
      */
     public $appConfig = '@yii/boost/tests/config.php';
 
-    public function testConstruct()
+    public function testConstruct1()
     {
+        $model = new TestForm;
+        $exception = new InvalidModelException($model);
+        $this->assertEquals('Invalid Model', $exception->getName());
+        $this->assertSame($model, $exception->getModel());
+        $this->assertSame('', $exception->getMessage());
+        $this->assertSame(0, $exception->getCode());
+        $this->assertSame(null, $exception->getPrevious());
+    }
+
+    public function testConstruct2()
+    {
+        $model = new TestForm;
+        $previous = new Exception;
+        $exception = new InvalidModelException($model, 'Error', 2, $previous);
+        $this->assertEquals('Invalid Model', $exception->getName());
+        $this->assertSame($model, $exception->getModel());
+        $this->assertSame('Error', $exception->getMessage());
+        $this->assertSame(2, $exception->getCode());
+        $this->assertSame($previous, $exception->getPrevious());
+    }
+
+    public function testModelDebugData1()
+    {
+        $model = $this->getMock(TestForm::className());
+        $this->assertInstanceOf('yii\boost\tests\TestForm', $model);
+        $model->expects($this->once())->method('hasErrors')->willReturn(false);
+        $model->expects($this->once())->method('getAttributes')->willReturn(['key1' => 'value1']);
+        $model->expects($this->never())->method('getErrors');
+        /* @var $model TestForm */
+        $exception = new InvalidModelException($model);
+        $this->assertEquals([
+            'class' => get_class($model),
+            'attributes' => ['key1' => 'value1']
+        ], $exception->getModelDebugData());
+    }
+
+    public function testModelDebugData2()
+    {
+        $model = $this->getMock(TestForm::className());
+        $this->assertInstanceOf('yii\boost\tests\TestForm', $model);
+        $model->expects($this->once())->method('hasErrors')->willReturn(true);
+        $model->expects($this->once())->method('getAttributes')->willReturn(['key1' => 'value1']);
+        $model->expects($this->once())->method('getErrors')->willReturn(['key2' => 'value2']);
+        /* @var $model TestForm */
+        $exception = new InvalidModelException($model);
+        $this->assertEquals([
+            'class' => get_class($model),
+            'attributes' => ['key1' => 'value1'],
+            'errors' => ['key2' => 'value2']
+        ], $exception->getModelDebugData());
     }
 }
