e2138e525ef8b2f701a2c661f234a115ff81edb0
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-07-16 08:34:56 +0300

    add fixColumns()

--- db/Migration.php
+++ db/Migration.php
@@ -2,6 +2,8 @@
 
 namespace yii\boost\db;
 
+use yii\db\mysql\ColumnSchemaBuilder;
+use yii\db\Schema;
 use yii\db\Migration as YiiMigration;
 
 class Migration extends YiiMigration
@@ -15,13 +17,64 @@ class Migration extends YiiMigration
     /**
      * @inheritdoc
      */
+    public function init()
+    {
+        parent::init();
+        $db = $this->getDb();
+        if ($db->getDriverName() == 'mysql') {
+            $queryBuilder = $db->getQueryBuilder();
+            $queryBuilder->typeMap = array_merge($queryBuilder->typeMap, [
+                Schema::TYPE_UPK => str_replace('(11)', '(10)', $queryBuilder->typeMap[Schema::TYPE_UPK]),
+                'tinyint' => 'tinyint(4)',
+                'utinyint' => 'tinyint(3)',
+                'usmallint' => 'smallint(5)',
+                'uinteger' => 'int(10)'
+            ]);
+        }
+    }
+
+    /**
+     * @param array $columns
+     * @return array
+     */
+    public function fixColumns(array $columns)
+    {
+        $closure = function () {
+            /* @var $this ColumnSchemaBuilder */
+            if ($this->isUnsigned) {
+                switch ($this->type) {
+                    case 'tinyint':
+                        $this->type = 'utinyint';
+                        break;
+                    case Schema::TYPE_SMALLINT:
+                        $this->type = 'usmallint';
+                        break;
+                    case Schema::TYPE_INTEGER:
+                        $this->type = 'uinteger';
+                        break;
+                }
+            } elseif ($this->type == Schema::TYPE_BOOLEAN) {
+                $this->isUnsigned = true;
+            }
+        };
+        foreach ($columns as $column) {
+            if ($column instanceof ColumnSchemaBuilder) {
+                call_user_func($closure->bindTo($column, get_class($column)));
+            }
+        }
+        return $columns;
+    }
+
+    /**
+     * @inheritdoc
+     */
     public function createTable($table, $columns, $options = null)
     {
         if (($this->getDb()->getDriverName() == 'mysql') && is_null($options)) {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $options = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
-        parent::createTable($table, $columns, $options);
+        parent::createTable($table, $this->fixColumns($columns), $options);
     }
 
     /**
