69b5e2760be7e46ea8cf3e9f43646e78411878ec
Ivan Chistyakov <ivan.chkv@gmail.com>
2016-07-30 01:46:45 +0300

    hotfix createTable()

--- db/Migration.php
+++ db/Migration.php
@@ -76,7 +76,7 @@ class Migration extends YiiMigration
      */
     public function createTable($table, $columns, $options = null)
     {
-        if (is_null($options) && in_array($db->getDriverName(), ['mysql', 'mysqli'])) {
+        if (is_null($options) && in_array($this->getDb()->getDriverName(), ['mysql', 'mysqli'])) {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
             $options = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
         }
